c232c6f0e61d68852390c4783e861d54af34bbac
Adeyemi Adekanye <sundayadeyemi8@gmail.com>
2021-06-17 04:12:19 +0100

    first commit attempt on crud

---
+++ my_blog/blog/views.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.shortcuts import render
+from .models import Post
+
+
+# Create your views here.
+
+def index(request):
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts' : posts
+    }
+
+    return render(request, "blog/index.html", context)
